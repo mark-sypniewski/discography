6fc29ae680eff11bd615591e87c93d31dfdc2c22
Mark Sypniewski <103773011+mark-sypniewski@users.noreply.github.com>
2022-11-06 22:13:59 -0500

    Add files via upload

---
+++ src/main/java/com/example/discography/DiscographyApplication.java
@@ -0,0 +1,13 @@
+package com.example.discography;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscographyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscographyApplication.class, args);
+	}
+
+}
