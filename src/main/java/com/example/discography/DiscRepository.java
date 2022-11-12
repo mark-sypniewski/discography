dd677c28c69427b80b69539baef2c4132f2baeb7
Mark Sypniewski <103773011+mark-sypniewski@users.noreply.github.com>
2022-11-12 13:30:26 -0500

    repository

--- src/main/java/com/example/discography/DiscRepository.java
+++ src/main/java/com/example/discography/DiscRepository.java
@@ -1,4 +1,23 @@
 package com.example.discography;
 
+import org.springframework.stereotype.Repository;
+
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.Map;
+
+@Repository
 public class DiscRepository {
+
+    Map<Long, Disc> discsList = new HashMap<>();
+
+    public DiscRepository() {
+        Disc sampleDisc = new Disc(1234L, "sampleArtist", "sampleAlbum", "sampleImageUrl", "sampleCategory", "sampleTracklist");
+
+        discsList.put(sampleDisc.getId(), sampleDisc);
+    }
+
+    public Disc findOne(Long id) {return discsList.get(id);}
+
+    public Collection<Disc> findAll() {return discsList.values();}
 }
