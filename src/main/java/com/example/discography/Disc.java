2e319fde0dd7c93b5330e201e9c221104bada43a
Mark Sypniewski <103773011+mark-sypniewski@users.noreply.github.com>
2022-11-12 13:18:22 -0500

    model

--- src/main/java/com/example/discography/Disc.java
+++ src/main/java/com/example/discography/Disc.java
@@ -1,4 +1,29 @@
 package com.example.discography;
 
 public class Disc {
+
+    private Long id;
+    private String artist;
+    private String album;
+    private String imageUrl;
+    private String category;
+    private String tracklist;
+
+    public Long getId() {return id;}
+    public String getArtist() {return artist;}
+    public String getAlbum() {return album;}
+    public String getImageUrl() {return imageUrl;}
+    public String getCategory() {return category;}
+    public String getTracklist() {return tracklist;}
+
+
+
+    public Disc(Long id, String artist, String album, String imageURL, String category, String tracklist) {
+        this.id = id;
+        this.artist = artist;
+        this.album = album;
+        this.imageUrl = imageURL;
+        this.category = category;
+        this.tracklist = tracklist;
+    }
 }
