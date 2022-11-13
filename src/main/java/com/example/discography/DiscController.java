98ccee5df9b60109388d3e867397a968f0a8c33d
Mark Sypniewski <103773011+mark-sypniewski@users.noreply.github.com>
2022-11-12 23:43:13 -0500

    controller

--- src/main/java/com/example/discography/DiscController.java
+++ src/main/java/com/example/discography/DiscController.java
@@ -1,4 +1,32 @@
 package com.example.discography;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.annotation.Resource;
+
+@Controller
 public class DiscController {
+
+    @Resource
+    private DiscRepository discRepo;
+
+    @RequestMapping("/discs")
+    public String findAllDiscs(Model model) {
+        model.addAttribute("discsModel", discRepo.findAll());
+        return "discsTemplate";
+    }
+
+    @RequestMapping("/disc")
+    public String findOneDisc(@RequestParam(value = "id") Long id, Model model) throws DiscNotFoundException {
+        if(discRepo.findOne(id) == null) {
+            throw new DiscNotFoundException();
+        }
+
+        model.addAttribute("discModel", discRepo.findOne(id));
+        return "discTemplate";
+    }
+
 }
